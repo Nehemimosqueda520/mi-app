c09fa917189664739fdbf57dab2d1ee0c213efef
unknown <nehemimosqueda8@gmail.com>
2024-01-30 01:37:14 -0300

    agregue lo principal

---
+++ src/components/Testimonium.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function Testimonium () {
+    return (
+        <div className="container-testimonium">
+            <img 
+            className="img-testimonium" 
+            src= {require("../img/Emma.png")}
+            alt= "foto de Ema"/>
+            <div className="testimonium-text-container">
+                <p className="name-&-country">Emma Bostian in Sweden</p>
+                <p className="work">Software Engineer at Spotify</p>
+                <p className="testimonium-text">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
