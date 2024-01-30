42853e410c221e67a472162540c6005049fbd948
unknown <nehemimosqueda8@gmail.com>
2024-01-30 14:31:53 -0300

    maqueta terminada

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import { Testimonium } from './components/Testimonium';
 function App() {
   return (
     <div className="App">
-      <h1>Here is what our alumni say about freeCodeCamp:</h1>
+      <h1>DLC Team</h1>
       <div class= "principal-container">
-      <Testimonium />
+      <Testimonium 
+      name='Nehemías Mosqueda'
+      university='UNRaf'
+      work='Programmer'
+      image='Sarah'
+      text="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." 
+      />
+      <Testimonium 
+      name='Camila Renna'
+      university='UNRaf'
+      work='2D Artist'
+      image='Emma'
+      text="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." 
+      />
+      <Testimonium 
+      name='Luka Martina'
+      university='UNRaf'
+      work='Game Designer'
+      image='Shawn'
+      text="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." 
+      />
+
       </div>
     </div>
   );
--- src/components/Testimonium.jsx
+++ src/components/Testimonium.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "../css/Testimonium.css";
 
-export function Testimonium () {
+export function Testimonium (props) {
     return (
         <div className="container-testimonium">
             <img 
             className="img-testimonium" 
-            src= {require("../img/Emma.png")}
-            alt= "foto de Ema"/>
+            src= {require(`../img/${props.image}.png`)}
+            alt= "foto de Ema" />
             <div className="testimonium-text-container">
-                <p className="name-and-country">Emma Bostian in Sweden</p>
-                <p className="work">Software Engineer at Spotify</p>
-                <p className="testimonium-text">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+                <p className="name-and-uni"><strong>{props.name}</strong> from <strong>{props.university}</strong></p>
+                <p className="work">{props.work}</p>
+                <p className="testimonium-text"> "{props.text}"
                 </p>
 
             </div>
