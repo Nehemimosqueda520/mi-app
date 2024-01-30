53bd558854c9838078ebc9eb9232db5152414a10
unknown <nehemimosqueda8@gmail.com>
2024-01-30 02:07:04 -0300

    agregue estilo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Testimonium } from './components/Testimonium';
 function App() {
   return (
     <div className="App">
+      <h1>Here is what our alumni say about freeCodeCamp:</h1>
+      <div class= "principal-container">
       <Testimonium />
-
+      </div>
     </div>
   );
 }
--- src/components/Testimonium.jsx
+++ src/components/Testimonium.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/Testimonium.css";
 
 export function Testimonium () {
     return (
@@ -8,7 +9,7 @@ export function Testimonium () {
             src= {require("../img/Emma.png")}
             alt= "foto de Ema"/>
             <div className="testimonium-text-container">
-                <p className="name-&-country">Emma Bostian in Sweden</p>
+                <p className="name-and-country">Emma Bostian in Sweden</p>
                 <p className="work">Software Engineer at Spotify</p>
                 <p className="testimonium-text">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
                 </p>
